c553f62a3163f97e5b970daf998e26a4781c9c55
Kiiiu15 <kiiiu15@users.noreply.github.com>
2019-08-22 12:40:19 -0300

    First PHP

---
+++ Pruebas/22.08/Sitio_Web/a.php
@@ -0,0 +1,14 @@
+<?php
+        
+       /* echo "Hola mundo";
+        echo "<div> Holaaaaaaaaaaaaaaaaaaaaaaa</div>"; //<--! nole gusta al rpofe-->
+        $numero=1;
+        $numero="Hola";
+        $numero = [
+            "nombre" /*clave del valor *//* => "Adrian","dni"=>"42567984"
+        ];
+
+     
+        //var_dump($_GET);*/
+        print_r($_POST);
+?>

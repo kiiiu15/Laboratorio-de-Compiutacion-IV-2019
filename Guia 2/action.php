77a2ca460ebde0113245e852c0b3565c2647d3c2
Kiiiu15 <kiiiu15@users.noreply.github.com>
2019-08-29 12:45:15 -0300

    Guia 2
    ponele

--- Guia 2/action.php
+++ Guia 2/action.php
@@ -3,11 +3,12 @@
 
     if ($_SERVER['REQUEST_METHOD'] == "POST")
         $parameters = $_POST;
-    else
+    else{
         $parameters = $_GET;
-
+    }
     foreach($parameters as $parameterName=>$parameterValue)
     {
         echo $parameterName . ': ' . $parameterValue . '<br>' ;
     }
+
 ?>

8e9d32bd089a58fb1cf90ff7d38164e105c85b7b
kiiiu15 <44479717+kiiiu15@users.noreply.github.com>
2019-09-14 17:47:30 -0300

    Ejercico 1 y 2

--- Guia3.1/index.php
+++ Guia3.1/index.php
@@ -23,8 +23,48 @@ echo "<br/> <hr/> <br/>";
 //c.	Convertir en mayúscula el primer caracter de cada nombre y pasarlo a un nuevo arreglo llamado $newArray. Mostrar por pantalla.
 $newArray=[];
 foreach ($arreglo as $nombre){
-    ucfirst($nombre);
-    array_push($newArray, $nombre);
+   
+    array_push($newArray,  ucfirst($nombre));
 }
 
+var_dump ($newArray);
+
+echo "<br/> <hr/> <br/>";
+
+
+// d.	Obtener la cantidad de valores de $newArray. Crear un nuevo arreglo con enteros llamado $keys. Obtener la cantidad del mismo. Chekeando ambas cantidades colocadas en dos variables, utilizar la función array_combine() para generar un nuevo arreglo llamado $arrayAssociative.Las cantidades deben ser iguales.
+    $cantidadValores = count($newArray);
+
+    $keys = [];
+
+    for ($i = 1; $i <= $cantidadValores; $i++){
+        $keys []= $i;
+    }
+    
+
+    $cantidadKeys = count($keys);
+    $arrayAssociative= ( $cantidadValores == $cantidadKeys ) ? array_combine ($keys , $newArray) : null ;
+
+// e.	Mostrar por pantalla $ arrayAssociative..
+
+    var_dump ($arrayAssociative);
+
+    echo "<br/> <hr/> <br/>";
+
+// 2.	Hacer una función que reciba como parámetros un valor y un array y mostrar por pantalla un mensaje que informe si ese valor existe o no en el array. Verificar resultado llamando a dicha función.
+
+    function estaEnArray ($array , $valor) {
+        foreach ($array as $value) {
+            if ($value == $valor) {
+                return true;
+            }
+        }
+        return false;
+    } 
+
+    echo (estaEnArray ($newArray, "Andrea")) ?  "Si esta"  : "No esta" ;
+
+    echo "<br/> <hr/> <br/>";
+
+    
 ?>
